afbaa2be110ff185e1efc75c6a521ccbbad281da
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2021-02-12 16:11:55 +0500

    Deploy settings

--- Radius/settings.py
+++ Radius/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'lddhx!qy$sahty_@j7d6!$f+1ebx!3c0fpf2zfgznamk7@iwzo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['radius66.herokuapp.com']
 
 
 # Application definition
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
